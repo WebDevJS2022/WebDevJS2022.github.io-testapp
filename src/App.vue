b78d10c837a536cf8dd03c2835db25e04c2bfd99
WeDev <webdevjs2022@gmail.com>
2023-07-09 16:10:51 +0500

    Создание компонента AppUsers.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,14 @@
 <template>
   <AppHeader />
+  <AppUsers />
 </template>
 
 <script>
 import AppHeader from './components/AppHeader.vue';
+import AppUsers from './components/AppUsers.vue';
 
 export default {
   name: 'App',
-  components: { AppHeader }
+  components: { AppHeader, AppUsers }
 };
 </script>
---
+++ src/components/AppUsers.vue
@@ -0,0 +1,49 @@
+<template>
+    <v-table class="users">
+      <thead>
+        <tr>
+          <th class="text-left">
+            Имя сотрудника
+          </th>
+          <th class="text-left">
+            Должность
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr
+          v-for="item in users"
+          :key="item.name"
+        >
+          <td>{{ item.name }}</td>
+          <td>{{ item.position }}</td>
+        </tr>
+      </tbody>
+    </v-table>
+    <v-pagination
+      v-model="page"
+      :length="pageSize"
+      :total-visible="4"
+    ></v-pagination>
+  </template>
+
+<script>
+import users from '@/data/users'
+
+export default {
+  data () {
+    return {
+      page: 1,
+      pageSize: 3,
+      users,
+    };
+  },
+}
+</script>
+
+<style>
+.users {
+ margin-top: 100px;
+ margin-left: 50px;
+}
+</style>
